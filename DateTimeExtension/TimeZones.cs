0d2decda54391993c96784138580cbbf07131464
akgjohansson <johansson.mcquack@gmail.com>
2017-09-20 16:24:15 +0200

    Lade till GetTimeDifference

---
+++ DateTimeExtension/TimeZones.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExtension
+{
+    public class TimeZones
+    {
+        public static int GetTimeDifference()
+        {
+            return DateTime.Now.ToLocalTime().Hour - DateTime.Now.ToUniversalTime().Hour;
+        }
+    }
+}

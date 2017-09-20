229149e62342ed07597c624ff34eea441e309db0
akgjohansson <johansson.mcquack@gmail.com>
2017-09-20 10:23:19 +0200

    Created Class CountDays with method TotalWeekDays

---
+++ DateTimeExtension/CountDays.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExtension
+{
+    public class CountDays
+    {
+        public int GetTotalWeekDays(DateTime today, DateTime endDay)
+        {
+            int totalWeekDays = 0;
+            int totalDays = Convert.ToInt32(Math.Round((endDay - today).TotalDays));
+
+            DateTime nextDay = Convert.ToDateTime(today.ToShortDateString());
+            for (int i = 0; i <= totalDays; i++)
+            {
+                nextDay = nextDay.AddDays(1);
+                if (nextDay.DayOfWeek != DayOfWeek.Sunday && nextDay.DayOfWeek != DayOfWeek.Saturday)
+                {
+                    totalWeekDays++;
+                }
+            }
+            return totalWeekDays;
+        }
+    }
+}
